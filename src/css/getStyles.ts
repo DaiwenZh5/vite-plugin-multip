03b72129dc2aabda0e08873bcfdd92456a75d899
vclemenzi <vclemenzi@users.noreply.github.com>
2024-03-29 19:55:24 +0100

    feat: support for `sass`, `less`

--- src/css/getStyles.ts
+++ src/css/getStyles.ts
@@ -2,7 +2,7 @@ import glob from "tiny-glob";
 import { resolve } from "../utils/resolve";
 
 export const getStyles = async (path: string): Promise<string[]> => {
-  const files = await glob(`${path}/*.css`);
+  const files = await glob(`${path}/*.{css,scss,sass,less}`);
   const styles = files.filter((file) => !file.includes("layout"));
 
   return styles.map((file) => {
