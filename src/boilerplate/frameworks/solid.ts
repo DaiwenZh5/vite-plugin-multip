9746658c60680de4134b74fe67200fe36356b0c2
vclemenzi <vclemenzi@users.noreply.github.com>
2024-04-14 15:35:48 +0200

    fix(dev)!: paths on windows

--- src/boilerplate/frameworks/solid.ts
+++ src/boilerplate/frameworks/solid.ts
@@ -1,11 +1,12 @@
 import { generateImports } from "../../assets/generateImports";
+import { fixPath } from "../../utils/path";
 
-export const solid = (file: string, css: string[], scripts: string[], dev: boolean): string => {
+export const solid = (file: string, css: string[], scripts: string[], dev: boolean, root: string): string => {
   return `
     <div id="app"></div>
     <script type="module">
       import { render } from '${!dev ? 'solid-js/web' : './node_modules/solid-js/web'}';
-      import App from '${file}';
+      import App from '${!dev ? file : fixPath(file, root)}';
       ${generateImports(css)}
       ${generateImports(scripts)}
 
--- src/boilerplate/frameworks/vue.ts
+++ src/boilerplate/frameworks/vue.ts
@@ -1,13 +1,14 @@
 import { generateImports } from "../../assets/generateImports";
+import { fixPath } from "../../utils/path";
 
-export const vue = (file: string, css: string[], scripts: string[], dev: boolean, init?: string): string => {
+export const vue = (file: string, css: string[], scripts: string[], dev: boolean, root: string, init?: string): string => {
   scripts = scripts.filter((script) => script.endsWith("init.ts") === false);
 
   return `
     <div id="app"></div>
     <script type="module">
       import { createApp } from '${!dev ? 'vue' : './node_modules/vue'}';
-      import App from '${file}';
+      import App from '${!dev ? file : fixPath(file, root)}';
       ${init ? `import { init } from '${init}';` : ""}
       ${generateImports(css)}
       ${generateImports(scripts)}
